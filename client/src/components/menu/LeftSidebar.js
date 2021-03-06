6d7a1efde516b1d32a3003ef8d0d4989e3dbf7a7
Diallo <elhadj-mamadou-foula.diallo@etu.univ-cotedazur.fr>
2021-03-06 20:37:41 +0100

    feat(client): display username and role on menu

--- client/src/components/menu/LeftSidebar.js
+++ client/src/components/menu/LeftSidebar.js
@@ -5,6 +5,9 @@ import { Nav, NavItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
 
 import { clearAuth } from '../../lib/authentication';
+import { getUser } from '../../lib/localstorage/userStorage';
+
+const { username, role } = getUser();
 
 const LeftSidebar = ({ children, className, ...props }) => (
 	<div style={{ overflowY: 'auto' }} {...props} className={clsx(className, 'min-vh-100 vw-100 d-flex')}>
@@ -12,15 +15,18 @@ const LeftSidebar = ({ children, className, ...props }) => (
 			vertical
 			className={clsx('vh-100 bg-dark py-4')}
 			style={{
-				width: 100,
+				width: 200,
 				left: 0,
 			}}
 		>
+			<small className="py-4 text-info justify-content-center w-100 d-flex">
+				{username?.toUpperCase()} : {role}
+			</small>
 			<NavItem className="text-center w-100 py-2">
-				<NavLink className="text-light" to="/home"><small>Assignements</small></NavLink>
+				<NavLink className="text-light" to="/home"><span>Assignements</span></NavLink>
 			</NavItem>
 			<NavItem className="text-center w-100 py-2">
-				<NavLink className="text-light" onClick={clearAuth} to="/login"><small>Log out</small></NavLink>
+				<NavLink className="text-light" onClick={clearAuth} to="/login"><span>Log out</span></NavLink>
 			</NavItem>
 		</Nav>
 		<div style={{ overflowY: 'auto' }} className="vh-100 vw-100 bg-light">
