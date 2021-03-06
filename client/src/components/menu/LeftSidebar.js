6102971569a43cb4235c30cc40314139632cca97
Diallo <elhadj-mamadou-foula.diallo@etu.univ-cotedazur.fr>
2021-03-06 22:49:59 +0100

    fix(client): LeftSidebar

--- client/src/components/menu/LeftSidebar.js
+++ client/src/components/menu/LeftSidebar.js
@@ -7,33 +7,34 @@ import { NavLink } from 'react-router-dom';
 import { clearAuth } from '../../lib/authentication';
 import { getUser } from '../../lib/localstorage/userStorage';
 
-const { username, role } = getUser();
-
-const LeftSidebar = ({ children, className, ...props }) => (
-	<div style={{ overflowY: 'auto' }} {...props} className={clsx(className, 'min-vh-100 vw-100 d-flex')}>
-		<Nav
-			vertical
-			className={clsx('vh-100 bg-dark py-4')}
-			style={{
-				width: 200,
-				left: 0,
-			}}
-		>
-			<small className="py-4 text-info justify-content-center w-100 d-flex">
-				{username?.toUpperCase()} : {role}
-			</small>
-			<NavItem className="text-center w-100 py-2">
-				<NavLink className="text-light" to="/home"><span>Assignements</span></NavLink>
-			</NavItem>
-			<NavItem className="text-center w-100 py-2">
-				<NavLink className="text-light" onClick={clearAuth} to="/login"><span>Log out</span></NavLink>
-			</NavItem>
-		</Nav>
-		<div style={{ overflowY: 'auto' }} className="vh-100 vw-100 bg-light">
-			{children}
+const LeftSidebar = ({ children, className, ...props }) => {
+	const { username, role } = getUser();
+	return (
+		<div style={{ overflowY: 'auto' }} {...props} className={clsx(className, 'min-vh-100 vw-100 d-flex')}>
+			<Nav
+				vertical
+				className={clsx('vh-100 bg-dark py-4')}
+				style={{
+					width: 200,
+					left: 0,
+				}}
+			>
+				<small className="py-4 text-info justify-content-center w-100 d-flex">
+					{username?.toUpperCase()} : {role}
+				</small>
+				<NavItem className="text-center w-100 py-2">
+					<NavLink className="text-light" to="/home"><span>Assignements</span></NavLink>
+				</NavItem>
+				<NavItem className="text-center w-100 py-2">
+					<NavLink className="text-light" onClick={clearAuth} to="/login"><span>Log out</span></NavLink>
+				</NavItem>
+			</Nav>
+			<div style={{ overflowY: 'auto' }} className="vh-100 vw-100 bg-light">
+				{children}
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 LeftSidebar.propTypes = {
 	children: PropTypes.node,
