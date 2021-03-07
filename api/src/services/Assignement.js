59287be64418753b6332b300785f7ed9aee38749
Swabahadine <swabmed@gmail.com>
2021-03-07 20:47:08 +0100

    fix(api): fix update assignment service

--- api/src/services/Assignement.js
+++ api/src/services/Assignement.js
@@ -6,6 +6,6 @@ exports.findById = (_id) => Assignment.findOne({ _id }).populate('subject');
 
 exports.findAll = (arg) => Assignment.find(arg).populate('subject');
 
-exports.update = async (_id, props) => Assignment.updateOne({ _id }, { $set: props });
+exports.update = async (_id, props) => Assignment.findOneAndUpdate({ _id }, { $set: props });
 
 exports.delete = (id) => Assignment.deleteOne({ _id: id });
