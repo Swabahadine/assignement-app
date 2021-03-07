0dedb560bf292fee5001f15ebdf34cde22adbef8
Swabahadine <55782824+Diallo-ucad@users.noreply.github.com>
2021-03-07 16:56:37 +0100

    feat(api): update Assignment model

--- api/src/models/Assignment.js
+++ api/src/models/Assignment.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const AssignmentSchema = new mongoose.Schema({
-    id: Number,
-    dateDeRendu: Date,
-    nom: String,
-    rendu: Boolean
+    dateDeRendu: { type: Date, required: true },
+    nom: {type: String, required: true},
+    rendu: {type: Boolean, default: false },
+    author: {type: String, required: true},
+    subject: { type: mongoose.Schema.Types.ObjectId, ref: 'Subject', required: true },
+    report: { type: String },
+    note: { type: Number, min:0, max:20 },
 });
 
 module.exports = mongoose.model('Assignment', AssignmentSchema);
